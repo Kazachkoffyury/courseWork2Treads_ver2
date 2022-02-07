1aaf1f4c79037411e751a4e9c87d9d85d951e0c6
Kazackoffyury <kazachkoff.yury@yandex.ru>
2022-02-07 20:31:45 +0300

    fix#2

--- src/test/java/com/example/coursework2treads_ver2/Constants.java
+++ src/test/java/com/example/coursework2treads_ver2/Constants.java
@@ -11,6 +11,7 @@ public class Constants {
     public static final String ANSWER2 = "ANSWER2";
     public static final Question QUEST1 = new Question(QUESTION1, ANSWER1);
     public static final Question QUEST2 = new Question(QUESTION2, ANSWER2);
+    //comment
 
 
 }
