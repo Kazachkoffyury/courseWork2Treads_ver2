332f0bc8f3846c36f69f6a92cd1fb3b85b8cfd6a
Kazackoffyury <kazachkoff.yury@yandex.ru>
2022-02-07 19:39:58 +0300

    Initial commit

---
+++ src/test/java/com/example/coursework2treads_ver2/CourseWork2TreadsVer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coursework2treads_ver2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWork2TreadsVer2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
